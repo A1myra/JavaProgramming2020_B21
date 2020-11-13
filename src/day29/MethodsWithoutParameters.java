2432696ee17b468987240101b1766c566df83a2c
Almira H <amiraa1225@gmail.com>
2020-11-13 10:59:03 -0500

    day29

---
+++ src/day29/MethodsWithoutParameters.java
@@ -0,0 +1,28 @@
+package day29;
+
+public class MethodsWithoutParameters {
+
+    public static void main(String[] args) {
+
+        printHello5Times();
+        System.out.println("Almira");
+        printHello5Times();
+        System.out.println("Cybertek School");
+        printHello5Times();
+
+        MethodsWithoutParameters.printHello5Times();
+    }
+
+    //AccessModifier  specifier   returnTYPE   MethodName (Parameter){
+    public static void printHello5Times(){
+        for(int i=1; i<=6; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+/*    //step1: print hello 5 times  (for loop)
+        //        step2: print your name
+        //        step3: print hello 5 times  (for loop)
+        //        step4: print your school name
+        //        step5: print hello 5 times   (for loop
+ */
---
+++ src/day29/Task1.java
@@ -0,0 +1,30 @@
+package day29;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello");
+        }
+
+        System.out.println("Almira");
+
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello");
+        }
+
+        System.out.println("Cybertek School");
+
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello");
+        }
+    }
+}
+
+/*    //step1: print hello 5 times  (for loop)
+        //        step2: print your name
+        //        step3: print hello 5 times  (for loop)
+        //        step4: print your school name
+        //        step5: print hello 5 times   (for loop
+ */
