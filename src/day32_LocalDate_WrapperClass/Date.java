43d93f3ff2878de98c5c56623781138d9aeb3df8
Almira H <amiraa1225@gmail.com>
2020-11-23 11:54:06 -0500

    day31

--- src/day32_LocalDate_WrapperClass/Date.java
+++ src/day32_LocalDate_WrapperClass/Date.java
@@ -17,6 +17,23 @@ public class Date {
         System.out.println(yesterday.isLeapYear()); // Leap ===> true, otherwise ===> false.
 
         LocalDate today = LocalDate.now();
+
         System.out.println(today);
+
+        LocalDate DofB= LocalDate.of(1999,2,23);
+
+        System.out.println(DofB.isLeapYear());
+
+        String[] Classmates = {"rinat","almira","daniyar","ayni","epan"};
+        LocalDate[] DateOfBirth = {
+                LocalDate.of(1997,11,17),
+                LocalDate.of(1999,2,23),
+                LocalDate.of(2000,12,12),
+                LocalDate.of(1998,8,12),
+                LocalDate.of(1996,2,4)};
+
+        for(int i=0; i<=Classmates.length-1; i++){
+            System.out.println(Classmates[i]+" : "+DateOfBirth[i]);
+        }
     }
 }
