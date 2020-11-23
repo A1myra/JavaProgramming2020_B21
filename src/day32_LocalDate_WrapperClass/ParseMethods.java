fba139120c52525d96f6001102291310f1bae490
Almira H <amiraa1225@gmail.com>
2020-11-23 16:26:14 -0500

    day32

--- src/day32_LocalDate_WrapperClass/ParseMethods.java
+++ src/day32_LocalDate_WrapperClass/ParseMethods.java
@@ -8,8 +8,33 @@ public class ParseMethods {
         System.out.println(data1+3);//1233
 
         int num1 = Integer.parseInt(data1); // none boxing
-        //           primitive == primitive
+        //           primitive <== primitive int
 
-        Integer num2 = Integer.parseInt(data1);
+        Integer num2 = Integer.parseInt(data1); //Autoboxing
+        //wrapper class <== primitive
+
+        System.out.println(num1+3);
+
+        String s1 = "Hello";
+
+        //int num3 = Integer.parseInt(s1);
+        //System.out.println(num3);
+
+        String s2 = "2.5";
+
+        double d1 = Double.parseDouble(s2); //none boxing
+                // primitive<===primitive
+        Double d2=Double.parseDouble(s2); //Autoboxing
+        //Wrapper class<==  primitive
+        System.out.println(d1+1);
+
+
+        String s3 = "true";
+        boolean b1=Boolean.parseBoolean(s3); // none boxing
+        Boolean b2 = Boolean.parseBoolean(s3); //Autoboxing
+        System.out.println(b1);
+        System.out.println(!b1);
+
+
     }
 }
---
+++ src/day32_LocalDate_WrapperClass/ValueOfMethods.java
@@ -0,0 +1,22 @@
+package day32_LocalDate_WrapperClass;
+
+public class ValueOfMethods {
+
+    public static void main(String[] args) {
+        String s1 = "123";
+        int num1 = Integer.valueOf(s1); //unboxing
+        //primitive <===== Wrapper class
+        Integer num2 = Integer.valueOf(s1); // none
+        System.out.println(num1+2);
+
+
+        String s2 = "2.5";
+        double num3 = Double.valueOf(s2);
+        System.out.println(num3+1);
+
+
+        String s3 = "TRUE"; // if I give other anything than true, it will always return false
+        boolean r1 = Boolean.valueOf(s3); //unboxing
+        System.out.println(r1);
+    }
+}
