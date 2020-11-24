3b3c6b5ff5664a332730a3367ed2021236834676
Almira H <amiraa1225@gmail.com>
2020-11-24 14:53:54 -0500

    day33

--- src/day33_ArrayList/ListPractice1.java
+++ src/day33_ArrayList/ListPractice1.java
@@ -21,5 +21,35 @@ public class ListPractice1 {
 
         System.out.println("Last odd number: "+odd.get(odd.size()-1));
         System.out.println("Last even number: "+even.get(even.size()-1));
+
+        System.out.println("==========================================================");
+
+        int[] arr1 = {1,2,3,4,5,6};
+        int[] arr2 = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr3 ={90,80,70,60};
+        int[] arr4 = { 40,35,45,65,7,5};
+        int[] arr5 = {35,54,67,89,98,76};
+
+        int [][]arr2D={arr1,arr2,arr3,arr4,arr5};
+
+        ArrayList<Integer> List1 = new ArrayList<>();
+
+        for(int  [] each1DA : arr2D){
+            for(int eachElement: each1DA){
+                List1.add(eachElement);
+            }
+        }
+       /* 下面的简单 上面的recommended
+       for( int each: arr1){
+            List1.add(each); //[1,2,3,4,5,6]
+        }
+        for(int each: arr2){
+            List1.add(each); //[1,2,3,4,5,6,1,2,3,4,5,6,7,8,9,10]
+        }
+
+         */
+
+
+        System.out.println(List1); //[1,2,3,4,5,6,1,2,3,4,5,6,7,8,9,10]
     }
 }
