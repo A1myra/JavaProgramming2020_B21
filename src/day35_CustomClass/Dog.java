101fdca42525004549d3b61481e88099e7f84d5b
Almira H <amiraa1225@gmail.com>
2020-12-01 15:33:17 -0500

    day35

--- src/day35_CustomClass/Dog.java
+++ src/day35_CustomClass/Dog.java
@@ -1,4 +1,32 @@
 package day35_CustomClass;
 
+import java.time.LocalDate;
+
 public class Dog {
+
+    public String nickName;
+    public String breed;
+    public String size;
+    public char gender;
+    public String color;
+    public LocalDate DofB;
+
+    public void setInfo(String dogNickname, String dogBreed,String dogSize,
+                        char dogGender,String dogColor,LocalDate dogDateOfBirth){
+        nickName=dogNickname;
+        breed=dogBreed;
+        size=dogSize;
+        gender=dogGender;
+        color=dogColor;
+        DofB=dogDateOfBirth;
+
+    }
+
+    public void getInfo(){
+        System.out.println(nickName+" "+breed+" "+size+" "+gender+" "+color+" "+DofB);
+    }
+
+
+
+
 }
