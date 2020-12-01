3082b8ccf3016532b673c67dead9ef80d8c7d91f
Almira H <amiraa1225@gmail.com>
2020-12-01 15:16:59 -0500

    day35

--- src/day35_CustomClass/CarMax.java
+++ src/day35_CustomClass/CarMax.java
@@ -1,5 +1,8 @@
 package day35_CustomClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CarMax {
 
     public static void main(String[] args) {
@@ -12,8 +15,30 @@ public class CarMax {
         Car car5 = new Car();
 
         car1.setInfo("Toyota","Camry","White",2017,50000);
-        car1.getInfo();
+        //car1.getInfo();
 
+        car2.setInfo("Honda","CR-V","Black",2015,15000.55);
+        //car2.getInfo();
 
+        car3.setInfo("Nissan","R32 GTR","DARK RED",1991,55000.99);
+
+        car4.setInfo("Range Rover","HSE","White",2019,98000);
+
+        car5.setInfo("Audi","R8","White",2020,105000);
+
+        Car[] cars = {car1,car2,car3,car4,car5};
+
+        for(Car eachCar : cars){
+            eachCar.getInfo();
+        }
+
+        System.out.println("===============================================");
+        ArrayList<Car> carList= new ArrayList<>(Arrays.asList(cars));
+
+        carList.removeIf(p -> p.price < 25000); //remove all the cars that are under 25k
+        for(int i=0; i<=carList.size()-1; i++){
+            carList.get(i).getInfo();
+
+        }
     }
 }
