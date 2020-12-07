111e8a2d85d4a6bd8c924b1aceed17ff26c7abd4
Almira H <amiraa1225@gmail.com>
2020-12-07 12:48:47 -0500

    day38

--- src/day38_StaticKeyword/StudentsObject.java
+++ src/day38_StaticKeyword/StudentsObject.java
@@ -5,13 +5,24 @@ public class StudentsObject {
     public static void main(String[] args) {
 
         CybertekStudent student1 = new CybertekStudent();
+        student1.setInfo("ayse",18,233,'F');
+
+        System.out.println(student1);
+
 
         CybertekStudent student2 = new CybertekStudent();
 
-        System.out.println(student1.schoolName);
-        System.out.println(student2.schoolName);
+        //System.out.println(student1.schoolName);
+        //System.out.println(student2.schoolName);
 
         System.out.println(CybertekStudent.schoolName); // preferred way to call
+
+
+
+
+
+
+
 
     }
 }
