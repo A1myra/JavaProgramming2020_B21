ae39d01764a4c7f61f434f760781b51f42f7052c
Almira H <amiraa1225@gmail.com>
2020-12-09 14:34:48 -0500

    office hours 12/09

---
+++ src/Office_Hours/Practice_12_09_2020/CybertekStudents.java
@@ -0,0 +1,26 @@
+package Office_Hours.Practice_12_09_2020;
+
+public class CybertekStudents {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public static String schoolName = "Cybertek School" , dreamJob = "SDET";;
+    public static boolean hasReplIt = true , hasFlipGrid = true;
+
+    public void setInfo(String name, int age, char gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+
+    public String toString() {
+        return "CybertekStudents{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +", School Name=" + schoolName+
+                ", dream job="+dreamJob+
+                '}';
+    }
+}
