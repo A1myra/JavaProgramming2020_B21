703234e1562ebee7eff2b16156e3639c58c8709f
Almira H <amiraa1225@gmail.com>
2020-12-14 12:53:24 -0500

    day 41

---
+++ src/day41_Encapsulation/Credential.java
@@ -0,0 +1,13 @@
+package day41_Encapsulation;
+
+public class Credential {
+
+    private String userName = "cybertek";
+    private String passWord = "cybertek123";
+
+    public String getUserName(){
+        return userName;
+    }
+
+
+}
---
+++ src/day41_Encapsulation/Login.java
@@ -0,0 +1,11 @@
+package day41_Encapsulation;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        Credential obj = new Credential();
+
+        System.out.println(obj.getUserName());
+    }
+}
