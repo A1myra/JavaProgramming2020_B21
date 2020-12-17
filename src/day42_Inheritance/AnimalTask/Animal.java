514efd39d2a1deaf83c6b91661638ba0240c5c48
Almira H <amiraa1225@gmail.com>
2020-12-17 11:33:58 -0500

    day 42

---
+++ src/day42_Inheritance/AnimalTask/Animal.java
@@ -0,0 +1,22 @@
+package day42_Inheritance.AnimalTask;
+
+public class Animal {
+
+    public String breed, color, name, size;
+    public char gender;
+    public int age;
+
+    public void eat (String food){
+        System.out.println(name+" is eating "+food);
+    }
+
+    public void drink(String drink){
+        System.out.println(name+" is drinking "+drink);
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+
+}
