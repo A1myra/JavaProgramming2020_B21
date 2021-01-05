ce37f578ba836a3551f733466df17bd805b7fd67
Almira H <amiraa1225@gmail.com>
2021-01-05 10:43:59 -0500

    day 45

---
+++ src/day45_Exceptions/PhoneTask/Device.java
@@ -0,0 +1,31 @@
+package day45_Exceptions.PhoneTask;
+
+public class Device {
+
+    public String brand, model;
+    public final String country;
+    public double price;
+
+    public Device(String brand, String model, String country, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.country = country;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", country='" + country + '\'' +
+                ", price= $" + price +
+                '}';
+    }
+    }
+
+/*
+Phone Task:
+            creata a class called device:
+                attributes: brand, model, price, country
+                methods: toString
+ */
---
+++ src/day45_Exceptions/PhoneTask/phone.java
@@ -0,0 +1,29 @@
+package day45_Exceptions.PhoneTask;
+
+public class phone extends Device {
+
+    public phone(String brand, String model, String country, double price) {
+        super(brand, model, country, price);
+    }
+
+    public String toString() {
+        return "phone"+super.toString();
+    }
+
+    public void call(long number){
+        System.out.println(brand+" "+model+" is calling "+number);
+    }
+
+    public void text(long number){
+        System.out.println(brand+" "+model+" is texting "+number);
+    }
+
+}
+
+/*
+create a class called phone:
+                attributes: brand, model, price, country
+                methods: call, text,  toString
+ */
+
+
