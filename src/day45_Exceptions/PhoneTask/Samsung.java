a3902e3fd6b9f2a66dd2e5d26d80395cca2b6d95
Almira H <amiraa1225@gmail.com>
2021-01-05 11:38:18 -0500

    day 45

--- src/day45_Exceptions/PhoneTask/Samsung.java
+++ src/day45_Exceptions/PhoneTask/Samsung.java
@@ -1,15 +1,15 @@
 package day45_Exceptions.PhoneTask;
 
-import day42_Inheritance.DeviceTask.Phone;
+public final class Samsung extends phone {
 
-    public final class Samsung extends Phone {
+            public Samsung(String model, double price) {
+                super("Samsung", model, "South Korea", price);
+            }
 
-        public Samsung(String model, double price) {
-            super("Samsung", model, "South Korea", price);
+            public void freeze() {
+                System.out.println(brand + " " + model + " is freezing");
+            }
+
         }
 
-        public void freeze() {
-            System.out.println(brand + " " + model + " is freezing");
-        }
 
-    }
