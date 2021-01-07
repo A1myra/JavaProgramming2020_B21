09709cf01cc8acc3e777ee644310b374aab59686
Almira H <amiraa1225@gmail.com>
2021-01-07 10:18:47 -0500

    day 45

--- src/day45_Exceptions/FinallyBlock.java
+++ src/day45_Exceptions/FinallyBlock.java
@@ -5,11 +5,13 @@ public class FinallyBlock {
     public static void main(String[] args) {
 
         try{
-            System.out.println("hello World");
-        }catch (RuntimeException e){
+            System.out.println(9 / 0);
+        }catch (IndexOutOfBoundsException e){
             System.out.println("Catch Block");
         }finally {
-
+            System.out.println("Finally: Closed");
         }
+
+        System.out.println("Test Completed");
     }
 }
---
+++ src/day45_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,15 @@
+package day45_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowsKeyword {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        //System.out.println("Cybertek".charAt(500));
+
+        System.out.println("Step 2");
+    }
+}
