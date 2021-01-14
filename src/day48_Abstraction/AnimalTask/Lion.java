912c43c0ccf673c5b68d1d198ca1cce3e5b88393
Almira H <amiraa1225@gmail.com>
2021-01-14 11:22:34 -0500

    day 48

---
+++ src/day48_Abstraction/AnimalTask/Lion.java
@@ -0,0 +1,17 @@
+package day48_Abstraction.AnimalTask;
+
+public class Lion extends Animal {
+    public Lion(String name, String breed, String size, char gender, int age) {
+        super("Lion", breed, size, gender, age);
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    @Override
+    public void hunt() {
+
+    }
+}
